5a27ad8c5aeee173cbe350d8bd4c3f8cc6116025
Ivan <igusev4444@gmail.com>
2026-02-06 22:57:16 +0300

    add allure and config cli

--- src/api/client.py
+++ src/api/client.py
@@ -1,4 +1,13 @@
 import requests
+import allure
+
+
+def _attach_allure(url:str, response: requests.Response, json: dict | None = None, params: dict | None = None):
+    allure.attach(url, name='requests URL', attachment_type=allure.attachment_type.TEXT)
+    allure.attach(str(params), name='Request params', attachment_type=allure.attachment_type.TEXT)
+    allure.attach(str(json), name='Request params', attachment_type=allure.attachment_type.JSON)
+    allure.attach(str(response.status_code), name='Response status', attachment_type=allure.attachment_type.TEXT)
+    allure.attach(str(response.text[:2000]), name='Response body first 2000 chars', attachment_type=allure.attachment_type.TEXT)
 
 
 class ApiClient:
@@ -8,6 +17,18 @@ class ApiClient:
         self.session = requests.Session()
         self.session.headers.update({'Accept': 'application/json'})
 
-    def get(self, path:str, params:dict=None) -> requests.Response:
+    def get(self, path:str, params:dict=None, raise_for_status: bool = True) -> requests.Response:
         url = f'{self.base_url}/{path.lstrip("/")}'
-        return self.session.get(url, params=params, timeout=self.timeout)
+        response = self.session.get(url, params=params, timeout=self.timeout)
+        _attach_allure(url, response, params=params)
+        if raise_for_status:
+            response.raise_for_status()
+        return response
+
+    def post(self, path:str, json:dict | None = None, raise_for_status: bool = True) -> requests.Response:
+        url = f'{self.base_url}/{path.lstrip("/")}'
+        response = self.session.post(url, json=json, timeout=self.timeout)
+        _attach_allure(url, response, json=json)
+        if raise_for_status:
+            response.raise_for_status()
+        return response
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,28 @@
 import pytest
 from src.api.client import ApiClient
 
-BASE_URL = 'https://httpbin.org'
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--base-url",
+        action="store",
+        default="https://httpbin.org",
+        help="Base URL for API requests",
+    ),
+    parser.addoption(
+        "--timeout",
+        default=10,
+        type=int,
+        help="Timeout for API requests",
+    )
 
 @pytest.fixture
 def user_data():
     return {'name': 'Ivan', 'age': 25}
 
+
 @pytest.fixture
-def api_client():
-    return ApiClient(BASE_URL)
+def api_client(request):
+    base_url = request.config.getoption('--base-url')
+    timeout = request.config.getoption('--timeout')
+    return ApiClient(base_url=base_url, timeout=timeout)
